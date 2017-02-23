e19186b90cee2821b1d00082326eba3522de80ee
Pawar <shruti.b.pawar@capgemini.com>
2017-02-23 15:08:46 +0530

    BankApplication

---
+++ BankApplicationTDD/src/com/capgemini/test/AccountServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.capgemini.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.junit.Assert.assertEquals;
+import com.capgemini.exceptions.InsufficientInitialAmountException;
+import com.capgemini.model.Account;
+import com.capgemini.repository.AccountRepository;
+import com.capgemini.service.AccountService;
+import com.capgemini.service.AccountServiceImpl;
+import static org.mockito.Mockito.when;
+public class AccountServiceImplTest {
+
+	@Mock
+	AccountRepository accountRepository;
+	
+	AccountService accountService;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		accountService = new AccountServiceImpl(accountRepository);
+	}
+
+	/*
+	 * create account
+	 * 1.when the amount is less than 500 system should throw exception
+	 * 2.when the valid info is passed account should be created successfully
+	 */
+
+	
+	
+	@Test(expected=com.capgemini.exceptions.InsufficientInitialAmountException.class)
+	public void whenTheAmountIsLessThanFiveHundredSystemShouldThrowException() throws InsufficientInitialAmountException
+	{
+		accountService.createAccount(101, 300);
+	}
+	
+	@Test
+	public void whenTheValidInfoIsPassedAccountShouldBeCreatedSuccessfully() throws InsufficientInitialAmountException
+	{
+		Account account =new Account();
+		account.setAccountNumber(101);
+		account.setAmount(5000);
+		
+		when(accountRepository.save(account)).thenReturn(true);
+		
+		assertEquals(account, accountService.createAccount(101, 5000));
+		
+	}
+}
